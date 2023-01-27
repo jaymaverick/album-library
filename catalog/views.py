7332a52232d03968bff937c365b9d2a2025b9dbf
jaymaverick <117550425+jaymaverick@users.noreply.github.com>
2023-01-27 23:51:18 +0200

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -157,7 +157,23 @@ class BookDelete(PermissionRequiredMixin, DeleteView):
 
 
 def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect("main:homepage")
+
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+            return render(request = request,
+                          template_name = "main/register.html",
+                          context={"form":form})
+
     form = UserCreationForm
     return render(request = request,
-                  template_name = "catalog/register.html",
+                  template_name = "main/register.html",
                   context={"form":form})
